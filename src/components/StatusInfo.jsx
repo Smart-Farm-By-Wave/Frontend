f098935b6be6d50e56dcd9b7ca2cbf424d1e809f
Nat <73571113+pangpondwee@users.noreply.github.com>
2023-03-02 23:03:06 +0700

    Edit status card's state

--- src/components/StatusInfo.jsx
+++ src/components/StatusInfo.jsx
@@ -1,11 +1,34 @@
 import happyIcon from '../assets/happy.png'
+import fireIcon from '../assets/fire.png'
 import styles from './StatusInfo.module.css'
+import classNames from 'classnames'
 
 function StatusInfo({ gasStatus, flameStatus }) {
-  if (gasStatus || flameStatus) {
+  if (flameStatus && gasStatus) {
     return (
-      <div>
-        <div>Hi</div>
+      <div className={styles.statusContainer}>
+        <img src={fireIcon} className={styles.statusIcon} />
+        <div className={classNames(styles.statusText, styles.alertText)}>
+          Flame and Gas is detected!
+        </div>
+      </div>
+    )
+  } else if (flameStatus) {
+    return (
+      <div className={styles.statusContainer}>
+        <img src={fireIcon} className={styles.statusIcon} />
+        <div className={classNames(styles.statusText, styles.alertText)}>
+          Flame is detected!
+        </div>
+      </div>
+    )
+  } else if (gasStatus) {
+    return (
+      <div className={styles.statusContainer}>
+        <img src={fireIcon} className={styles.statusIcon} />
+        <div className={classNames(styles.statusText, styles.alertText)}>
+          Gas is detected!
+        </div>
       </div>
     )
   } else {
