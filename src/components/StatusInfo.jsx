6b83a0ff04d3337388b6f959fc019865700e7f44
Nat <73571113+pangpondwee@users.noreply.github.com>
2023-03-11 17:02:11 +0700

    Add fire alert popup

--- src/components/StatusInfo.jsx
+++ src/components/StatusInfo.jsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from 'react'
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
 
 import { getData } from '../fetchData'
 
@@ -7,14 +9,33 @@ import fireIcon from '../assets/fire.png'
 import styles from './StatusInfo.module.css'
 import classNames from 'classnames'
 
+const MySwal = withReactContent(Swal)
+
 function StatusInfo() {
   const [statusInfo, setStatusInfo] = useState({ smoke: false, fire: false })
+  const [isAlertShown, setIsAlertShown] = useState(false)
+
   useEffect(() => {
     const interval = setInterval(() => {
       getData('/central/fire', setStatusInfo)
     }, 1000)
     return () => clearInterval(interval)
   }, [])
+
+  // show fire alert
+  useEffect(() => {
+    if (!isAlertShown && (statusInfo.fire || statusInfo.smoke)) {
+      setIsAlertShown(true)
+      MySwal.fire({
+        title: 'Warning',
+        text: 'Flame or Gas is detected!',
+        icon: 'warning',
+        iconColor: '#f03e3e',
+      })
+    } else if (isAlertShown && !(statusInfo.fire || statusInfo.smoke)) {
+      setIsAlertShown(false)
+    }
+  }, [statusInfo, isAlertShown])
 
   if (statusInfo.fire && statusInfo.smoke) {
     return (
